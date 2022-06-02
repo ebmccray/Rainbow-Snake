848a24768378bdad7a31d2cafc736b9129e0e9ca
ebmccray <eriennemccray@gmail.com>
2022-06-02 10:53:00 -0700

    Player class initialized, movement incorporated.

--- snake.py
+++ snake.py
@@ -16,7 +16,7 @@ with open('traceback template.txt', 'w+') as f:
         # Window and Game Variables:
         WIDTH = 300
         HEIGHT = 300
-        FPS = 60
+        FPS = 3
 
         # COLORS:
         BLACK = (0, 0, 0)
@@ -35,6 +35,31 @@ with open('traceback template.txt', 'w+') as f:
 
         # CLASSES
 
+        class Snake(pygame.sprite.Sprite):
+            def __init__(self):
+                super().__init__()
+
+                self.size = 10
+
+                self.image = pygame.Surface((self.size,self.size))
+                self.image.fill(WHITE)
+                self.rect = self.image.get_rect(center = (WIDTH/2, HEIGHT/2))
+
+                self.snake_length = 0
+                self.current_direction = pygame.K_LEFT
+
+            def update(self):
+                match self.current_direction:
+                    case pygame.K_UP:
+                        self.rect.centery -= self.size
+                    case pygame.K_DOWN:
+                        self.rect.centery += self.size
+                    case pygame.K_LEFT:
+                        self.rect.centerx -= self.size
+                    case pygame.K_RIGHT:
+                        self.rect.centerx += self.size
+                
+
         # FUNCTIONS
         # General Game Functions:
         def DrawGame():
@@ -44,6 +69,8 @@ with open('traceback template.txt', 'w+') as f:
 
         # INITIALIZATION
         pygame.init()
+        player = Snake()
+        all_sprites.add(player)
 
         # Create window and define cclock
         window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -62,14 +89,23 @@ with open('traceback template.txt', 'w+') as f:
 
             # Check for user input
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
-                    key = event.key
-                    if key == pygame.K_RETURN:
-                        pass
-                    elif key == pygame.K_SPACE:
-                        paused = not paused
+                match event.type:
+                    case pygame.QUIT:
+                        running = False
+                    case pygame.KEYDOWN:
+                        match event.key:
+                            case pygame.K_RETURN:
+                                pass
+                            case pygame.K_SPACE:
+                                paused = not paused
+                            case pygame.K_UP:
+                                player.current_direction = event.key
+                            case pygame.K_DOWN:
+                                player.current_direction = event.key
+                            case pygame.K_RIGHT:
+                                player.current_direction = event.key
+                            case pygame.K_LEFT:
+                                player.current_direction = event.key
 
             # While not paused, run simulation
             if not paused:
